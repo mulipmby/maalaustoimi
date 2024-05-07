d94fcf78f378d0864ce813022249ef6f6bc7f548
mulipmby <paulus.muli@hotmail.com>
2024-05-08 00:07:13 +0300

    wepb images and styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,19 @@
+import "@/app/globals.css";
+
 import Nabvar from "@/components/navbar";
 import Footer from "../components/footer";
-import Carousel from "@/components/carousel";
 import Contact from "@/components/Contact";
-import "@/app/globals.css";
+
 import { ReactCompareSlider, ReactCompareSliderImage, ReactCompareSliderHandle } from 'react-compare-slider';
-import Link from "next/link";
 import Image from "next/image";
 
-import img1 from "/public/Takakuisti_ennen.jpg";
-import img2 from "/public/Takakuisti_jälkeen.jpg";
-import img3 from "/public/mökki_enne.jpg";
-import img4 from "/public/mökki_jälkeen.jpg";
-import img5 from "/public/etukuisti_enne.jpg";
-import img6 from "/public/etukuisti_jälkeen.jpg";
-import img7 from "/public/IMG_2644.jpg";
-import imageright from "/public/IMG_2739.jpg";
-import edi from "/public/EDI.jpg";
+import img_backporch from "/public/Takakuisti_jälkeen.webp";
+import imgbefore from "/public/etukuisti_enne.webp";
+import imgafter from "/public/etukuisti_jälkeen.webp";
+import img_whitehouse from "/public/IMG_2644.webp";
+import img_brownhouse from "/public/IMG_2739.webp";
+
 export default function Home() {
-  const images = [img1, img2, img3, img4, img5, img6];
   return (
     <main className="bg-white">
       <Nabvar />
@@ -28,39 +24,28 @@ export default function Home() {
               display: 'none'
             }} />}
             className="w-full md:w-3/5 h-full object-cover"
-            itemTwo={<ReactCompareSliderImage src={img5.src} alt="Jälkeen"/>}
-            itemOne={<ReactCompareSliderImage src={img6.src} alt="Ennen"/>}
+            itemTwo={<ReactCompareSliderImage loading="lazy" src={imgbefore.src} alt="Jälkeen"/>}
+            itemOne={<ReactCompareSliderImage loading="lazy" src={imgafter.src} alt="Ennen"/>}
           />
           {/* <Image
             src={edi}
             alt="Ennen"
             className="w-full md:w-1/2 h-full object-cover"
           /> */}
-          <div className="flex flex-col w-full md:w-2/5 h-full justify-center items-center gap-4 p-16">
+          <div className="flex flex-col w-full md:w-2/5 h-full justify-center items-center gap-4 px-4 py-16 md:p-16">
             <div className="flex flex-col gap-4">
               <h1 className="w-full text-3xl text-start mb-10 font-semibold text-teal-950">
-                Ulkomaalaukset yksityisille ja taloyhtiöille
+                MAALAUSTOIMI HYTÖNEN & ESKOLA 
               </h1>
               <div className="flex flex-col w-full h-full gap-4 text-start">
                 <p className="w-full">
-                  Ulkomaalauksen suunnittelu ja toteutus vaativat huolellista
-                  suunnittelua ja ammattitaitoa, erityisesti kun kyseessä ovat
-                  yksityiskodit tai taloyhtiöt. Haluamme tarjota sinulle
-                  täydellisen ratkaisun ulkoseinien kunnostukseen ja
-                  kaunistamiseen.
+                  Olemme 25-vuotias  aineenopettajaopiskelija Edvin Eskola ja 26-vuotias juuri valmistunut opettaja Antti Hytönen.
                 </p>
                 <p className="w-full">
-                  Olipa ulkomaalauksen tarve sitten kodin virkistämiseksi tai
-                  taloyhtiön julkisivun kohentamiseksi, voit luottaa meidän
-                  kokeneeseen tiimiimme. Työskentelemme yhteistyössä kanssasi
-                  varmistaaksemme, että lopputulos vastaa täysin odotuksiasi ja
-                  tarpeitasi.
+                  Monien vaiheiden jälkeen päädyimme talvella 2023 perustamaan maalausalan yrityksen.
                 </p>
                 <p className="w-full">
-                  Käytämme vain laadukkaita materiaaleja ja ammattitaitoista
-                  työvoimaa varmistaaksemme kestävän ja kauniin lopputuloksen.
-                  Ota yhteyttä meihin, niin suunnitellaan yhdessä juuri sinun
-                  tarpeisiisi sopiva ulkomaalausratkaisu!
+                  Päätös ryhtyä yrittäjäksi syntyi halusta toimia oikein ja tehdä huippu laadukasta työtä asiakkaan ehdoilla. 
                 </p>
               </div>
             </div>
@@ -69,13 +54,28 @@ export default function Home() {
         <div className="w-full flex flex-col justify-center items-center">
           <div className="flex flex-col w-full h-full justify-center items-center bg-white">
             <div className="w-full bg-gray-50">
-              <div className="px-4 w-full max-w-[1600px] m-auto relative h-full rounded-lg flex md:flex-row flex-col text-lg gap-6 md:gap-24 text-start justify-center items-center py-16">
-                <div className="w-full md:w-1/2">
-                  <Image src={img7} className="rounded-lg" alt="Ennen" />
+              <div className="px-4 w-full max-w-[1600px] m-auto  relative h-full rounded-lg flex md:flex-row flex-col gap-6 md:gap-24 text-start justify-center items-center py-16">
+                <div className="w-full md:w-1/2 relative">
+                  <div
+                    href="/huoltotyot"
+                    className="rounded-lg w-full h-full block"
+                  >
+                    <div className="relative inset-0 rounded-lg overflow-hidden">
+                      <Image
+                        src={img_whitehouse}
+                        alt=""
+                        style={{objectFit: "cover" }}
+                        className="rounded-lg h-[600px]"
+                      />
+                    </div>
+                    <div className="absolute bottom-0 left-0 w-full bg-black bg-opacity-50 p-4 text-white rounded-b-lg">
+                      <h2 className="text-xl font-bold">ULKOMAALAUS</h2>
+                    </div>
+                  </div>
                 </div>
-                <div className="w-full md:w-1/2 h-full flex flex-col gap-10">
-                  <h1 className="text-3xl font-semibold text-teal-950 text-start w-full">
-                    Miksi maalaustoimi?
+                <div className="w-full md:w-1/2 h-full flex flex-col gap-10 text-lg">
+                  <h1 className="text-3xl leading-relaxed font-semibold text-teal-950 text-start w-full">
+                      Täydellinen ulkomaalauspalvelu – Pidä kotisi tai taloyhtiösi julkisivu loistokunnossa
                   </h1>
                   <p className="w-full">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
@@ -87,45 +87,39 @@ export default function Home() {
               </div>
             </div>
             <div className="w-full bg-gray-100">
-              <div className="px-4 w-full max-w-[1600px] m-auto  relative h-full rounded-lg flex md:flex-row flex-col gap-6 md:gap-24 text-start justify-center items-center py-16">
+              <div className="px-4 w-full max-w-[1600px] m-auto  relative h-full rounded-lg flex md:flex-row flex-col-reverse gap-6 md:gap-24 text-start justify-center items-center py-16">
                 <div className="w-full md:w-1/2 h-full flex flex-col gap-8 text-lg">
                   <h1 className="text-3xl text-teal-950 font-semibold text-start w-full mb-6">
                     Miksi maalaustoimi?
                   </h1>
                   <div className="flex flex-col gap-3">
                     <h2 className="w-full font-medium text-lg text-teal-950 uppercase">
-                      Saat maksuttoman kartoituskäynnin
+                      LAADUSTA TINKIMÄTTÄ
                     </h2>
                     <p className="w-full">
-                      Tilaa meiltä asiantuntija paikalle kartoittamaan talosi
-                      maalaustarve sekä antamaan hinta-arvio ja alustava aikataulu
-                      talon maalauksesta. Saat puolueettoman arvion
-                      maalaustarpeesta, eikä käynti sido sinua vielä mihinkään.
+                      Ensimmäisenä prioriteettinamme on työn jäljen moitteettomuus ja asiakastyytyväisyys. Valmiin urakan jälkeen pinnan tulee kestää sään lisäksi tarkankin silmän tarkastelu.
                     </p>
                   </div>
                   <div className="flex flex-col gap-3">
                     <h2 className="w-full font-medium text-lg text-teal-950 uppercase">
-                      Ammattitaitoiset ja kokeneet maalarit
+                      ASIAKASTYYTYVÄISYYS
                     </h2>
                     <p className="w-full">
-                      Maalarimme ovat koulutettuja ja kokeneita maalausalan
-                      ammattilaisia, joilta maalaaminen sujuu nopeasti ja
-                      huolellisesti alusta loppuun.
+                      Laadun lisäksi tarjoamme kilpailukykyiset hinnat ja ystävällisen asiakaspalvelun.
                     </p>
                   </div>
                   <div className="flex flex-col gap-3">
                     <h2 className="w-full font-medium text-lg text-teal-950 uppercase">
-                      Laadukasta maalausjälkeä
+                      KOKEMUS
                     </h2>
                     <p className="w-full">
-                      Me emme tingi laadusta! Maalaamme kaikki ulkomaalaukset
-                      käsityönä pensselillä, jolloin varmistamme tasaisen,
-                      huolellisen ja viimeistellyn lopputuloksen.
+                      Kummallakin meistä on yli sadan ulkomaalauskohteen kartuttama kokemus.
+                      Lisäksi kuulumme ennakkoperintärekisteriin joten työstämme voi hakea kotitalousvähennyksiä.
                     </p>
                   </div>
                 </div>
                 <div className="w-full md:w-1/2">
-                  <Image src={img2} alt="Ennen" style={{objectFit: "cover", objectPosition: "bottom"}} className="rounded-lg h-[600px]" />
+                  <Image src={img_backporch} alt="Ennen" style={{objectFit: "cover", objectPosition: "bottom"}} className="rounded-lg h-[600px]" />
                 </div>
               </div>
             </div>
@@ -138,7 +132,7 @@ export default function Home() {
                   >
                     <div className="relative inset-0 rounded-lg overflow-hidden">
                       <Image
-                        src={imageright}
+                        src={img_brownhouse}
                         alt=""
                         style={{objectFit: "cover" }}
                         className="rounded-lg h-[600px]"
@@ -164,7 +158,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <Contact id="contact" info="Talon maalaus ensi kesänä? Pyydä tarjous nyt!" />
+          <Contact id="contact" info="Kaipaako sinun kotisi, autotallisi tai mökkisi uutta maalipintaa tai vanhan pinnan ehostusta?" />
         </div>
       </div>
       <Footer />
